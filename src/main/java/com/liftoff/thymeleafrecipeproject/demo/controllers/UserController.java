374a103a633a5a0c8aa981b4ceea0b0bbc1fffb1
Zackarias00 <zacharywarfelresume@gmail.com>
2021-08-24 17:20:59 -0500

    displayProfilePage added in UserController

---
+++ src/main/java/com/liftoff/thymeleafrecipeproject/demo/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.liftoff.thymeleafrecipeproject.demo.controllers;
+
+import com.liftoff.thymeleafrecipeproject.demo.data.RecipeRepository;
+import com.liftoff.thymeleafrecipeproject.demo.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/profile")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping
+    public String displayProfilePage(Model model){
+        model.addAttribute("title","My Profile");
+        return "profile";
+    }
+}
